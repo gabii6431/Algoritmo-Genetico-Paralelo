04562471adb138165b2084e562578fe526902a79
Natasha <nathy-ac@htomail.com>
2019-11-15 14:17:20 -0300

    Inicio metodo remoto
    Co-authored-by: Gabriela Garcia<gabii6431@gmail.com>
    Co-authored-by: LuzianeFreitas <freitas.lu@outlook.com>
    Co-authored-by: Giovanna Moreira <giovanna.verola2016@gmail.com>

---
+++ AG_Interdisciplinar/src/Migracao.java
@@ -0,0 +1,7 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+public interface Migracao extends Remote{
+    public void recebe(ArrayList<Individuo> individuos) throws RemoteException;
+}
